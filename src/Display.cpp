9216fdec4a034bc6d215f74ad24d7944bdb594d8
ladykiller374 <baingheielts@gmail.com>
2023-03-28 20:42:57 -0400

    plane visible

--- src/Display.cpp
+++ src/Display.cpp
@@ -22,16 +22,21 @@ Display::Display(){
 int Display::runDisplay() {
 	name_attach_t *attach;
 	all_planes data;
+	compsys_display_msg msg;
+
 	if((attach = name_attach(NULL, DISPLAY_ATTACH_POINT, 0)) == NULL){
 		printf("Display failed to create channel\n\n");
 		return EXIT_FAILURE;
 	}
 
 	while(1){
-		rcvid = MsgReceive(attach->chid, &data, sizeof(data), NULL); // receive messages from radar
+		rcvid = MsgReceive(attach->chid, &msg, sizeof(msg), NULL); // receive messages from radar
 		if (rcvid == -1) {/* Error condition, exit */
 			break;
 		}
+
+		data = msg.planes;
+//		printf("Hdrr %d\n\n", msg.planes.hdr.type);
 
 		if (rcvid == 0) {/* Pulse received */
 			switch (data.hdr.code) {
@@ -75,6 +80,7 @@ int Display::runDisplay() {
 		// check for appropriate header and copy the data to planes
 		if (data.hdr.type == 0x01){
 			planes = data.allPlanes;
+			printf("Dataaaaaa %d\n\n", data.allPlanes.size());
 //			for(plane_info i: planes){
 //				printf("plane id#%d; coords(%d,%d,%d)\n\n", i.ID, i.posX, i.posY, i.posZ);
 //			}
@@ -82,8 +88,10 @@ int Display::runDisplay() {
 		MsgReply(rcvid, EOK, 0, 0);
 		cTimer timer(5,0,5,0);
 		timer.wait_next_activation();
-		for(int i = 0; i <51; i++){
-			for (int j = 0; j < 102; j++){
+
+		std::cout << "X-Y view" << endl;
+		for(int i = 0; i < 75; i++){
+			for (int j = 0; j < 120; j++){
 				bool isprinted = false;
 				if (i == 0) {
 					std::cout << "-";
@@ -91,33 +99,65 @@ int Display::runDisplay() {
 				else if (j == 0) {
 					std::cout << "|";
 				}
-				else if (i == 50) {
+				else if (i == 74) {
 					std::cout << "-";
 				}
-				else if (j == 101) {
+				else if (j == 119) {
 					std::cout << "|";
 				}
 
-				else if (planes.size() != 0){
+				if (planes.size() != 0){
 					for(plane_info x: planes){
-						if(i == ((x.posX/2000)+1) && (j == ((x.posY/1000)+1))) {
+						if(i == ((x.posX/2000)+1) && (j == ((x.posY/2000)+1))) {
 							std::cout << x.ID;
 							isprinted = true;
 						}
 					}
 				}
-				if (!isprinted && i != 0 && j != 0 && i != 50 && j != 101) {
+				if (!isprinted && i != 0 && j != 0 && i != 74 && j != 119) {
 					std::cout << " ";
 				}
 			}
 			std::cout << "\n";
 		}
 
+		std::cout << "X-Z view" << endl;
+				for(int i = 0; i < 75; i++){
+					for (int j = 0; j < 120; j++){
+						bool isprinted = false;
+						if (i == 0) {
+							std::cout << "-";
+						}
+						else if (j == 0) {
+							std::cout << "|";
+						}
+						else if (i == 74) {
+							std::cout << "-";
+						}
+						else if (j == 119) {
+							std::cout << "|";
+						}
+
+						if (planes.size() != 0){
+							for(plane_info x: planes){
+								if(i == ((x.posX/2000)+1) && (j == ((x.posZ/2000)+1))) {
+									std::cout << x.ID;
+									isprinted = true;
+								}
+							}
+						}
+						if (!isprinted && i != 0 && j != 0 && i != 74 && j != 119) {
+							std::cout << " ";
+						}
+					}
+					std::cout << "\n";
+				}
 		for(plane_info y: planes) {
 			std::cout << "P" << y.ID << ": " << "\n";
 			//std::cout << "ID: " << ID.at(s) << "\n";
 			std::cout << "Position X: " << y.posX << "\n";
-			std::cout << "Position Y: " << y.posY << "\n" << "\n";
+			std::cout << "Position Y: " << y.posY << "\n";
+			std::cout << "Position Z: " << y.posY << "\n" << "\n";
 		}
 	}
 	name_detach(attach, 0);
