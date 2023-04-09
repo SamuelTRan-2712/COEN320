f46863a3bcadf9816401d15b5efe9e0a148e6b03
Keeano Gerald <keeanogerald@gmail.com>
2023-04-08 22:22:53 -0400

    Update Radar.cpp
    fixed timer. added partial print for position

--- src/Radar.cpp
+++ src/Radar.cpp
@@ -24,8 +24,10 @@ int Radar::toComputerSys(all_planes data){
 void Radar::pingAirspace(){
 	cTimer timer(1,0, 1, 0); //creating a timer of period 1 with an offset of 1. CHANGE IF WE WANT TO CHANGE THE AMOUNT OF TIME BETWEEN PINGS
 
-	msg msg;
-	msg.hdr.type = 0x00;
+
+
+	msg pos_msg;
+	pos_msg.hdr.type = 0x00;
 	plane_info rmsg;
 	all_planes data;
 	data.hdr.type = 0x01;
@@ -33,13 +35,11 @@ void Radar::pingAirspace(){
 
 
 	while (1){
+
+		timer.wait_next_activation();
+		cout << "Timer waited 1s" << endl;
 		// get the populated airspace
 		airspace = Plane::airspace;
-
-
-//		TODO: set timer to ping planes every X seconds using cTimer
-		//timer.waitTimer();
-		sleep(1);	// to be removed, for testing only
 
 
 		if (airspace.empty()){
@@ -52,10 +52,12 @@ void Radar::pingAirspace(){
 					printf("Radar: Failed connection to server %d\n\n", i);
 					break;
 				}
-				if (MsgSend(server_coid, &msg, sizeof(msg), &rmsg, sizeof(rmsg)) == -1){
+				printf("Client sending position %.2f m \n", pos_msg);
+				if (MsgSend(server_coid, &pos_msg, sizeof(pos_msg), &rmsg, sizeof(rmsg)) == -1){
 					printf("Radar: Failed to send message %d\n\n", i);
 					break;
 				}
+
 				name_close(server_coid);
 				//printf("Radar: Data of Plane #%d: Coords(%d, %d, %d)\n\n", rmsg.ID, rmsg.posX, rmsg.posY, rmsg.posZ);
 
