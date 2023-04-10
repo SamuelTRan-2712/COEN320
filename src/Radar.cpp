bb344b99730002772ee66eefa93715510b893571
Keeano Gerald <keeanogerald@gmail.com>
2023-04-10 17:52:39 -0400

    Changed Radar
    Fixed name_attach, partially fixed position print

--- src/Radar.cpp
+++ src/Radar.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 // ----------------------------------- Constants -----------------------------------
 #define COMPUTER_ATTACH_POINT "ComputerSystem"
+#define ATTACH_POINT "my_channel"
 
 
 // ----------------------------------- Class Methods -----------------------------------
@@ -24,22 +25,21 @@ int Radar::toComputerSys(all_planes data){
 void Radar::pingAirspace(){
 	cTimer timer(1,0, 1, 0); //creating a timer of period 1 with an offset of 1. CHANGE IF WE WANT TO CHANGE THE AMOUNT OF TIME BETWEEN PINGS
 
-	msg msg;
-	msg.hdr.type = 0x00;
+
+
+	msg pos_msg;
+	pos_msg.hdr.type = 0x00;
 	plane_info rmsg;
 	all_planes data;
 	data.hdr.type = 0x01;
-	char buffer[10];
-
+	char attach_points[10];
 
 	while (1){
+
+		timer.wait_next_activation();
+		cout << "Timer waited 1s" << endl;
 		// get the populated airspace
 		airspace = Plane::airspace;
-
-
-//		TODO: set timer to ping planes every X seconds using cTimer
-		//timer.waitTimer();
-		sleep(1);	// to be removed, for testing only
 
 
 		if (airspace.empty()){
@@ -48,14 +48,16 @@ void Radar::pingAirspace(){
 		else {
 			for (int i : airspace){
 				// go through the airspace and ping each plane
-				if ((server_coid = name_open(itoa(i,buffer,10), 0)) == -1){
+				if ((server_coid = name_attach(ATTACH_POINT , 0)) == -1){
 					printf("Radar: Failed connection to server %d\n\n", i);
 					break;
 				}
-				if (MsgSend(server_coid, &msg, sizeof(msg), &rmsg, sizeof(rmsg)) == -1){
+				printf("Client sending position %.2f m \n", pos_msg );
+				if (MsgSend(server_coid, &pos_msg, sizeof(pos_msg), &rmsg, sizeof(rmsg)) == -1){
 					printf("Radar: Failed to send message %d\n\n", i);
 					break;
 				}
+
 				name_close(server_coid);
 				//printf("Radar: Data of Plane #%d: Coords(%d, %d, %d)\n\n", rmsg.ID, rmsg.posX, rmsg.posY, rmsg.posZ);
 
